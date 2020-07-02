7f245f79ffdf46467c2eece25a03ea24c0c1f2a3
Naman <ng24645@gmai;.com>
2020-07-02 21:34:18 +0530

    Sign Up in work

--- TaxiRental/taxi/urls.py
+++ TaxiRental/taxi/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import include ,path
 from . import views
 
 urlpatterns = [
    path("",views.index,name="HomePage"),
    path("info/",views.info,name="Information"),
-   path("contact/",views.contact, name="Contact Us")
+   path("contact/",views.contact, name="Contact Us"),
+   path("signin/", views.sign_in, name="signin"),
+   path("callback", views.callback, name="callback")
 ]
--- TaxiRental/taxi/views.py
+++ TaxiRental/taxi/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from . models import Contact
+from django.urls import reverse
+from taxi.helper import get_user
+from taxi.auth_helper import get_sign_in_url, get_token_from_code, store_token, store_user, remove_user_and_token, get_token
+
 
 def index(request):
     return render(request,'taxi/index.html')
@@ -19,5 +23,25 @@ def contact(request):
         thank = True
     return render(request, 'taxi/contact.html', {'thank': thank})
 
-    
+def sign_in(request):
+  # Get the sign-in URL
+  sign_in_url, state = get_sign_in_url()
+  # Save the expected state so we can validate in the callback
+  request.session['auth_state'] = state
+  # Redirect to the Azure sign-in page
+  return HttpResponseRedirect(sign_in_url)
 
+def callback(request):
+  # Get the state saved in session
+  expected_state = request.session.pop('auth_state', '')
+  # Make the token request
+  token = get_token_from_code(request.get_full_path(), expected_state)
+
+  # Get the user's profile
+  user = get_user(token)
+
+  # Save token and user
+  store_token(request, token)
+  store_user(request, user)
+
+  return HttpResponseRedirect(reverse('taxi/'))
